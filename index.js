316348a0d7a83380a84b7cf3c40eee01eb8e4ced
araolive <85970126+araolive@users.noreply.github.com>
2024-04-13 15:19:46 -0400

    Add files via upload
    Adicionando efeitos no html com javaScript

---
+++ index.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.about',{
+    rotate:{x: 0, y: 80, z: 0},
+    duration: 4000});
+
+sr.reveal('.projects', {duration: 4000});
+
+sr.reveal('.main', {
+    rotate: {x: 100, y:100, z:0},
+    duration: 4000,
+
+})
